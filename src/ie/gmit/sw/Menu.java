124b8e39e0763140c9ad43ad53d5ce3c2b4bc113
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2019-12-06 22:00:22 +0000

    Add starter code

---
+++ src/ie/gmit/sw/Menu.java
@@ -0,0 +1,51 @@
+package ie.gmit.sw;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public final class Menu {
+    private String dataLoc;
+
+    public Menu() { }
+
+    public Menu(String dataLoc) throws FileNotFoundException {
+        setDataLoc(dataLoc);
+    }
+
+    public String getDataLoc() {
+        return dataLoc;
+    }
+
+    public void setDataLoc(String dataLoc) throws FileNotFoundException {
+        // Verify that the file exists
+        if (!new File(dataLoc).isFile()) {
+            throw new FileNotFoundException("Couldn't find file: "+ dataLoc);
+        }
+
+        this.dataLoc = dataLoc;
+    }
+
+    public void display() {
+        System.out.println("=============================================================================");
+        System.out.println("|                              Language Detector                            |");
+        System.out.println("=============================================================================");
+
+        if (dataLoc == null) {
+            String input;
+            try (var console = new Scanner(System.in)) {
+                do {
+                    System.out.print("$ Enter WiLi data location: ");
+                    try {
+                        input = console.nextLine().trim();
+                        setDataLoc(input);
+
+                        break;
+                    } catch (FileNotFoundException e) {
+                        System.out.println(OutColour.colourText("File not found. Try again.\n", OutColour.ERROR));
+                    }
+                } while (true);
+            }
+        }
+    }
+}
--- src/ie/gmit/sw/Runner.java
+++ src/ie/gmit/sw/Runner.java
@@ -2,6 +2,7 @@ package ie.gmit.sw;
 
 public class Runner {
     public static void main(String[] args) {
-
+        Menu menu = new Menu();
+        menu.display();
     }
 }
